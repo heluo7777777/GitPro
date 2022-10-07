e594c71b3d6d624ef0ff1a7449201ab6f2ddae9a
heluo7777777 <heluo7777777@gmail.com>
2022-10-07 22:12:43 +0800

    7 master commit

--- com/hl/service/DemoService.java
+++ com/hl/service/DemoService.java
@@ -3,14 +3,17 @@ package com.hl.service;
 public class DemoService {
 
     public String toUpperCase(String str){
+        str += "suffix";
         return str.toUpperCase();
     }
 
     public String toLowerCase(String str){
+        str += "prefix";
         return str.toLowerCase();
     }
 
     public String concatStr(String sourceStr, String targetStr){
+        sourceStr += "he";
         return sourceStr + targetStr;
     }
 }
--- com/hl/utils/CommonUtils.java
+++ com/hl/utils/CommonUtils.java
@@ -3,6 +3,7 @@ package com.hl.utils;
 public class CommonUtils {
 
     public static void println(Object obj){
+        System.out.println("test bra");
         System.out.println(obj);
     }
 }
